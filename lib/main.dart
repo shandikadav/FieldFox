56180e9b77ce5bcf2feebaa2eb0254296a16568e
shandikadav <shandikadavid.14@gmail.com>
2026-03-09 21:28:18 +0700

    feat(pubspec) : add new dependencies for development

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,3 @@
-import 'package:field_fox/pages/badminton_page.dart';
-import 'package:field_fox/pages/bantuan_page.dart';
-import 'package:field_fox/pages/basket_page.dart';
-import 'package:field_fox/pages/chat_page.dart';
-import 'package:field_fox/pages/favorit_page.dart';
-import 'package:field_fox/pages/futsal_page.dart';
-import 'package:field_fox/pages/gym_page.dart';
-import 'package:field_fox/pages/komunitas_page.dart';
-import 'package:field_fox/pages/signin_screen.dart';
-import 'package:field_fox/pages/main_page.dart';
-import 'package:field_fox/pages/menu_lapangan_page.dart';
-import 'package:field_fox/pages/notifikasi_page.dart';
-import 'package:field_fox/pages/renang_page.dart';
-import 'package:field_fox/pages/sepakbola_page.dart';
-import 'package:field_fox/pages/splash_screen.dart';
-import 'package:field_fox/pages/voli_page.dart';
 import 'package:field_fox/routes/route.dart';
 import 'package:flutter/material.dart';
 
